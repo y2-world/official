752e488eecba27c184eaad98a90effb9b1286ef3
y2world <yui92496@gmail.com>
2021-02-08 01:00:28 +0900

    Ver 1.0

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/news', function () {
+    return view('news');
+});
+Route::get('/profile', function () {
+    return view('profile');
+});
+Route::get('/works', function () {
+    return view('works');
+});
+Route::get('/music', function () {
+    return view('music');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+
