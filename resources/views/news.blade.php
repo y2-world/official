886be9d39c5bdea4baa22e870175c30a0d596571
y2world <yui92496@gmail.com>
2021-02-13 19:47:15 +0900

    Ver 3.7

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <h2>News</h2>
     <small class="date">2021.02.07</small>
-    <div class="link">Yuki Official Website 開設！</div>
+    <div class="link">Yuki Yoshida Official Website 開設！</div>
     <p class="text">Yukiのアーティスト活動やエンジニアとしての活動をお伝えするオフィシャルウェブサイトが開設されました。</p>  
     <hr> 
 </div>
