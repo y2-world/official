52e165ff7906cce90e5c3cc067602357226a5049
y2world <yui92496@gmail.com>
2021-04-21 21:23:27 +0900

    title changed

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Radio</title>
+        <title>y2_radio Form</title>
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
