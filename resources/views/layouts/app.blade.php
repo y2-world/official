5d9b2b9fa8abe40a4741feaa952e543922eef20e
y2world <yui92496@gmail.com>
2021-04-29 14:17:18 +0900

    qiita deleted

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -220,12 +220,9 @@
             <a class="nav-link" href="{{ url('/music') }}">&emsp;Music</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="{{ url('/radio') }}">&emsp;Radio</a>
+            <a class="nav-link" href="{{ url('/radio') }}">&emsp;Radio&emsp;</a>
             </li>
-            <li class="nav-item">
-            <a class="nav-link" href="https://qiita.com/y2_engineer" target="_blank">&emsp;Qiita&emsp;</a>
-            </li>
-            </div>
+            </div>            
             </nav>
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
